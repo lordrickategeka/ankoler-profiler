93b796c319f057a22f19f196f62978e5f8a094f7
lordrickategeka <loganatetech@gmail.com>
2025-12-10 11:14:01 +0300

    jjj

--- database/migrations/2025_10_07_133540_create_patient_records_table.php
+++ database/migrations/2025_10_07_133540_create_patient_records_table.php
@@ -36,7 +36,7 @@ return new class extends Migration
 
             $table->foreign('primary_care_unit_id')
                   ->references('id')
-                  ->on('organization_units')
+                  ->on('organizations')
                   ->onDelete('set null');
 
             $table->index('patient_number');
