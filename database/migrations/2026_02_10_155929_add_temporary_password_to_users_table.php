ef0c0fa044390f3a4c4f4fb52566b9b7369654e8
lordrickategeka <98752007+lordrickategeka@users.noreply.github.com>
2026-02-12 10:09:29 +0300

    update

--- database/migrations/2026_02_10_155929_add_temporary_password_to_users_table.php
+++ database/migrations/2026_02_10_155929_add_temporary_password_to_users_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::table('table_name', function (Blueprint $table) {
+        Schema::table('users', function (Blueprint $table) {
             $table->string('temporary_password')->nullable();
         });
     }
